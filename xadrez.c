92260878cc8fba79f1646978b1ce81051ed3c96c
Victor Gominho <89556454+VictorGominho@users.noreply.github.com>
2025-05-21 20:00:02 +0000

    Nível Mestre

--- xadrez.c
+++ xadrez.c
@@ -1,37 +1,70 @@
 #include <stdio.h>
 
-int main() {
+void torreMove(int casas) {
+     if (casas > 0) {
+          printf("Direita\n");
+          torreMove(casas - 1);
+     }   
+}
+
+void rainhaMove(int casas) {
+     if (casas > 0) {
+          printf("Esquerda\n");
+          rainhaMove(casas - 1);
+     }   
+}
+
+void bispoMove(int casas) {
+     int horizontal = 0, vertical = 0;
+     while (vertical != casas) {
+          printf("Cima ");
+          vertical++;
+          while (horizontal != vertical) {
+               printf("Direita\n");
+               horizontal++;
+          }
+     }
+}
+
+void cavaloMove(int vertical) {
+     int casas = 0, horizontal = 0;
      
-     int torreMove = 5, bispoMove = 5, rainhaMove = 8, cavaloMove1 = 2, cavaloMove2 = 1;
+     if (vertical > 2)
+     {
+          return;
+     }
+     
+     while (casas != 3)
+     {
+          printf("Cima\n");
+          casas++;
+          vertical--;
+          while (casas != 3 && vertical == 0)
+          {
+               printf("Direita\n");
+               casas++;
+               horizontal--;
+          }
+          
+     }
+     
+     
+
+}
+
+int main() {
 
      printf("### Xadrez ###\n\n");
      
      printf("Movimentos da Torre:\n"); // 5 para direita
-     for (torreMove; torreMove > 0; torreMove--) {
-          printf("Direita\n");
-     }
+     torreMove(5);
 
      printf("\nMovimentos do Bispo:\n"); // 5 na diagonal superior direita
-     while (bispoMove > 0)
-     {
-          printf("Cima Direita\n");
-          bispoMove--;
-     }
+     bispoMove(5);
 
      printf("\nMovimentos da Rainha:\n"); // 8 para esquerda
-     do
-     {
-          printf("Esquerda\n");
-          rainhaMove--;
-     } while (rainhaMove > 0);
+     rainhaMove(8);
 
-     printf("\nMovimentos do Cavalo:\n"); // 2 para baixo e 1 para esquerda
-     for (cavaloMove2; cavaloMove2 > 0; cavaloMove2--) {
-          while (cavaloMove1 > 0)
-          {
-               printf("Baixo\n");
-               cavaloMove1--;
-          }
-          printf("Esquerda\n");
-     }
+     printf("\nMovimentos do Cavalo:\n"); // 2 para cima e 1 para direita
+     cavaloMove(2); // Recebe número de movimentos na vertical
 }
